c184e3cbdd6ccc1b9455684d941ffab47f01347a
Nolla Technology <joshuasimon656@gmail.com>
2023-08-19 18:37:02 +0300

    Updated navbar and version number

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -5,18 +5,19 @@ import { faBars, faClose } from "@fortawesome/free-solid-svg-icons";
 
 export default function Navbar() {
   const links = [
+    { name: "Home", href: "home", key: "#home" },
+    { name: "About Us", href: "about-us", key: "#about-us" },
     { name: "Services", href: "services", key: "#services" },
-    { name: "Tools", href: "tools", key: "#tools" },
     { name: "Contact Us", href: "contact-us", key: "#contact-us" },
   ];
 
   const [open, setOpen] = useState(false);
 
   return (
-    <nav className="nav--bar py-4 px-6 md:flex items-center z-[2] md:justify-between mx-auto relative rounded-xl bg-slate-100 shadow-md">
+    <nav className="nav--bar py-2 px-6 md:flex items-center z-[2] md:justify-between mx-auto relative rounded-xl">
       <div className="flex items-center justify-between">
         <img
-          className="img max-w-[3rem]"
+          className="img max-w-[2.5rem]"
           src="assets/kibuti.png"
           alt="kibuti"
         />
@@ -27,13 +28,13 @@ export default function Navbar() {
         />
       </div>
       <ul
-        className={`nav--links md:flex z-[1] pl-6 md:z-auto md:static md:rounded-none absolute w-full md:w-auto left-0 gap-4 bg-slate-100 transition-all ease-linear duration-300 top-16 shadow-xl md:shadow-none ${
+        className={`nav--links bg-white md:flex z-[1] pl-6 md:z-auto md:static md:rounded-none absolute w-full md:w-auto left-0 gap-4 transition-all ease-linear duration-300 top-14 shadow-xl md:shadow-none ${
           open ? "top-16" : "left-[-800px]"
         }`}
       >
         {links.map((link) => (
           <li
-            className="my-6 md:my-0 md:mx-4 text-blue-900 hover:text-blue-600 transition ease-in duration-200"
+            className="my-6 md:my-0 md:mx-2 text-sm text-blue-900 hover:text-blue-600 transition ease-in duration-200"
             key={link.key}
           >
             <Link
